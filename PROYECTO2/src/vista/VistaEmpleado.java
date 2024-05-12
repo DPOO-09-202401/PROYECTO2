4c94fe16b3cfd4bae0956f6be1c4593568be9552
srodriguezo23 <s.rodriguezo23@uniandes.edu.co>
2024-05-12 09:28:20 -0500

    empleado

--- PROYECTO2/src/vista/VistaEmpleado.java
+++ PROYECTO2/src/vista/VistaEmpleado.java
@@ -1,5 +1,90 @@
 package vista;
 
-public class VistaEmpleado {
+import controlador.LogicaUsuarios;
+import modelo.ModeloRol;
+import modelo.ModeloUsuario;
+import utilidades.Utilidades;
 
+public class VistaEmpleado {
+    private LogicaUsuarios logicaUsuarios = new LogicaUsuarios();
+    private ModeloUsuario usuarioActual;
+
+    public static void main(String[] args) {
+        Utilidades.cargarDatos("./src/datos/");
+        VistaEmpleado vista = new VistaEmpleado();
+        Boolean continuar = true;
+        while (continuar) {
+            Boolean autenticado = vista.iniciarSesion();
+            if (autenticado) {
+                vista.ejecutarOpciones();
+            }
+        }
+    }
+
+    private Boolean iniciarSesion() {
+        System.out.println("--------------------");
+        System.out.println("BIENVENIDO A LA APP PARA EMPLEADOS DE GALERIANDES");
+        String correo = Utilidades.lectorConsola("Ingrese su correo: ");
+        String contrasena = Utilidades.lectorConsola("Ingrese su contraseña: ");
+        System.out.println("--------------------");
+        ModeloUsuario autenticado = logicaUsuarios.iniciarSesion(correo, contrasena, ModeloRol.EMPLEADO);
+        if (autenticado == null) {
+            System.out.println("Autenticacion fallida");
+            return false;
+        }
+        System.out.println("Bienvenido");
+        this.usuarioActual = autenticado;
+        return true;
+    }
+
+    private void imprimirOpciones() {
+        System.out.println("--------------------");
+        System.out.println("1. Consultar perfil");
+        System.out.println("2. Consultar piezas en venta");
+        System.out.println("3. Consultar piezas en subasta");
+        System.out.println("4. Recibir pago por pieza");
+        System.out.println("5. Registrar oferta en subasta");
+        System.out.println("6. Finalizar subasta");
+
+        System.out.println("7. Salir");
+        System.out.println("--------------------");
+    }
+
+    private void ejecutarOpciones() {
+        Boolean continuar = true;
+        while (continuar) {
+            imprimirOpciones();
+            Integer opcion = Integer.parseInt(Utilidades.lectorConsola("Ingrese opcion a escoger: "));
+            switch (opcion) {
+                case 1:
+                    this.imprimirPerfil();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    continuar = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void imprimirPerfil() {
+        ModeloUsuario u = this.usuarioActual;
+        System.out.println("-> " + u.getNombre() + ", " + u.getCorreo() + ", " + u.getRol() + ", " + u.getTelefono());
+    }
 }
