afd872308510b639ad4c3f374bd8c7b9504103b6
srodriguezo23 <s.rodriguezo23@uniandes.edu.co>
2024-05-12 08:27:41 -0500

    controlador

--- PROYECTO2/src/controlador/Logica.java
+++ PROYECTO2/src/controlador/Logica.java
@@ -1,5 +1,21 @@
 package controlador;
 
-public class Logica {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+
+public interface Logica<T> {
+    public T crearUno(T nuevo) throws Exception;
+
+    public T consultarUno(String identificador) throws Exception;
+
+    public HashMap<String, T> consultarTodos();
+
+    public T editarUno();
+
+    public void eliminarUno(String identificador) throws Exception;
+
+    public Integer cargarDatos(String ruta) throws FileNotFoundException, IOException;
+
 
 }
--- PROYECTO2/src/controlador/LogicaUsuarios.java
+++ PROYECTO2/src/controlador/LogicaUsuarios.java
@@ -1,5 +1,76 @@
 package controlador;
 
-public class LogicaUsuarios {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+
+import modelo.ModeloRol;
+import modelo.ModeloUsuario;
+import modelo.PersistenciaUsuario;
+
+public class LogicaUsuarios implements Logica<ModeloUsuario>{
+
+    private PersistenciaUsuario persistenciaUsuario = new PersistenciaUsuario();
+
+    public ModeloUsuario iniciarSesion(String correo, String contrasena, ModeloRol rol) {
+        ModeloUsuario modeloUsuario = PersistenciaUsuario.usuarios.get(correo);
+        if (modeloUsuario != null && modeloUsuario.getRol().equals(rol)) {
+            String thisContrasena = modeloUsuario.getContrasena();
+            if (thisContrasena.equals(contrasena)) {
+                return modeloUsuario;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public ModeloUsuario consultarUno(String identificador) throws Exception {
+        ModeloUsuario usuario =  PersistenciaUsuario.usuarios.get(identificador);
+        if (usuario == null){
+            throw new Exception ("Usuario no encontrado");
+        }
+        return usuario;
+    }
+
+    @Override
+    public HashMap<String, ModeloUsuario> consultarTodos() {
+        HashMap<String, ModeloUsuario> usuarios =  persistenciaUsuario.usuarios;
+        return usuarios;
+    }
+
+    @Override
+    public ModeloUsuario editarUno() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'editarUno'");
+    }
+
+    @Override
+    public void eliminarUno(String identificador) throws Exception {
+        if (! PersistenciaUsuario.usuarios.containsKey(identificador)){
+            throw new Exception("Usuario no encontrado");
+        }
+        PersistenciaUsuario.usuarios.remove(identificador);
+        persistenciaUsuario.actualizar();
+    }
+
+    @Override
+    public Integer cargarDatos(String ruta) throws FileNotFoundException, IOException {
+        persistenciaUsuario.cargar(ruta);
+        HashMap<String, ModeloUsuario> usuarios =  PersistenciaUsuario.usuarios;
+        return usuarios.size();
+
+    }
+
+    @Override
+    public ModeloUsuario crearUno(ModeloUsuario nuevoUsuario) throws Exception {
+        if (PersistenciaUsuario.usuarios.containsKey(nuevoUsuario.getContrasena())){
+            throw new Exception("Correo duplicado");
+        };
+        PersistenciaUsuario.usuarios.put(nuevoUsuario.getCorreo(), nuevoUsuario);
+        persistenciaUsuario.actualizar();
+        return nuevoUsuario;
+    }
+
+
 
 }
