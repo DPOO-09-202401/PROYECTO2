d6fe69a41e283d467ec2d5e85bd48138d39992e5
srodriguezo23 <s.rodriguezo23@uniandes.edu.co>
2024-06-01 12:22:27 -0500

    vistagrafica1

---
+++ src/vistaGrafica/NombresVistas.java
@@ -0,0 +1,18 @@
+package vistaGrafica;
+
+public enum NombresVistas {
+    LOGIN("LOGIN"),
+    ADMIN("ADMIN"),
+    USUARIO("CLIENTE"),
+    EMPLEADO("EMPLEADO");
+
+    private String nombre;
+
+    private NombresVistas(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
